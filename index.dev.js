70de3455483cf2dc97068f77458b8adab37e2b33
Sinan <me@wunnle.com>
2019-05-02 12:21:22 +0300

    storing env variables

--- index.dev.js
+++ index.dev.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const home = require('./routes')
+require('dotenv').config()
 
 const app = express()
 
 app.use(home)
-
 
 const port = 5000
 app.listen(port, () => console.log(`Server started on ${port}`))
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Welcome to mongodb test!')
+  res.send(`Welcome to mongodb test! Btw api key is ${process.env.MYAPIKEY}`)
 })
 
 module.exports = app
