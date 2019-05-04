ec09da42c6e87a8d7f5157e60457cb089f88bdff
Sinan <me@wunnle.com>
2019-05-04 09:25:09 +0300

    testing connecting with monggoose

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,22 @@
 const express = require('express')
-const mongo = require('mongodb').MongoClient;
+const monggoose = require('mongoose')
 
+const dbURL = process.env.MONGO_DB_URL
 const app = express()
+
+const kittySchema = new monggoose.Schema({
+  name: String
+})
 
 app.get('/', (req, res) => {
 
-  mongo.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true }, function (err, client) {
-    if (err) {
-      res.json({
-        message: 'Welcome to mongodb test! Something went wrong while connecting.',
-        status: 'fail'
-      })
-      console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
-    }
-    res.json({
-      message: 'Welcome to mongodb test! Connected to db!',
-      status: 'success'
-    })
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-  });
+  monggoose.connect(dbURL, { useNewUrlParser: true })
 
+  const db = monggoose.connection
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', () => {
+    res.send('connected to db!')
+  })
 
 })
 
