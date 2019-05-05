1d3cc64bdb83b4a9452dbf835203ba2b369a8891
Sinan <me@wunnle.com>
2019-05-05 22:27:33 +0300

    added cors tpp allUsers/

--- routes/allUsers/index.js
+++ routes/allUsers/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const { personSchema } = require('../../schemas')
 const { connect } = require('../../helpers/db-helper')
 
 const app = express()
+
+app.use(cors())
+app.options('*', cors());
 
 app.get('/allUsers/', async (req, res) => {
 
